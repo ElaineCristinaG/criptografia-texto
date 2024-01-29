e63dda864463c9702f7124fbff890bf5e9e62b67
elainecarminhola <elainecgcarminhola@gamil.com>
2024-01-29 11:20:54 -0300

    implementação da substituição do tempalte

---
+++ app.js
@@ -0,0 +1,28 @@
+
+console.log("OláMundo!")
+
+document.addEventListener("DOMContentLoaded",function(){
+
+    function alternaSection(){
+        const templatePadrao = document.getElementById("visibilidade");
+        const templateNovo = document.getElementById("textoVazio");
+        templatePadrao.style.display = "none";
+        templateNovo.style.display = "block";
+
+    }
+
+
+    
+    
+
+});
+
+
+// function codificar(){
+//  var textoEntrada = document.getElementById("textoEntrada").value;
+//  var textoCodificado = encodeURIComponent(textoEntrada);
+//  console.log(textoEntrada);
+
+
+
+
