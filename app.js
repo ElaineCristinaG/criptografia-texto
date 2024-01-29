4d1f3ffefd940ca5b20220f0bd9637cdcd80af9c
elainecarminhola <elainecgcarminhola@gamil.com>
2024-01-29 17:01:02 -0300

    implementação da função codifica e o comportamento da tela ok

--- app.js
+++ app.js
@@ -1,28 +1,91 @@
-
-console.log("OláMundo!")
-
 document.addEventListener("DOMContentLoaded",function(){
 
+    document.getElementById("texto_entrada").value = "";
+
+    const saida_texto_codificado = document.getElementById("saida_texto_codificado");
+    const texto_vazio = document.getElementById("texto_vazio");
+
+    saida_texto_codificado.style.display = "none";
+    texto_vazio.style.display = "block";
+
+    /**Sessão Codifica */
+    function codificar(){
+
+        const texto_entrada = document.getElementById("texto_entrada").value;
+        const texto_saida = texto_entrada
+        .replace(/a/g,"ai")
+        .replace(/e/g,"enter")
+        .replace(/i/g,"imes")
+        .replace(/o/g,"obter")
+        .replace(/u/g,"ufat")
+    
+        document.getElementById("texto_codificado").textContent = texto_saida;
+    }
+
     function alternaSection(){
-        const templatePadrao = document.getElementById("visibilidade");
-        const templateNovo = document.getElementById("textoVazio");
-        templatePadrao.style.display = "none";
-        templateNovo.style.display = "block";
+        
+        texto_vazio.style.display = "none";
+        saida_texto_codificado.style.display = "block";
+        codificar();
+    }
+
+    function verifica_texto_entrada(){
+        const verifica_texto = document.getElementById("texto_entrada").value;
+        if(verifica_texto != ""){
+            alternaSection()
+        }else{
+            alert("Digite um texto de entrada!")
+        }
+    }
+
+    const botao_codificar = document.getElementById("botao_codifica");
+    botao_codificar.addEventListener("click", alternaSection);
+
+    /**sessão decodifica */
+
+    function decodificar() {
+        const texto_codificado = document.getElementById("texto_codificado").textContent;
+        let texto_decodificado = texto_codificado
+            .replace(/ai/g, "a")
+            .replace(/enter/g, "e")
+            .replace(/imes/g, "i")
+            .replace(/obter/g, "o")
+            .replace(/ufat/g, "u");
+    
+        document.getElementById("texto_decodificado").textContent = texto_decodificado;
+    }
+    
+
+    function copiar_texto_codificado(){
+
+        const texto_codificado = document.getElementById("texto_codificado");
+        const inputElement = document.createElement("input");
+        inputElement.value = texto_codificado;
+        document.body.appendChild(inputElement);
+        inputElement.select();
+
+        navigator.clipboard.writeText(texto_codificado)
+        .then(() => {
+            alert("Texto copiado para a área de transferência!");
+        })
+        .catch(err => {
+            console.error('Erro ao copiar texto: ', err);
+        })
+        .finally(() => {
+            document.body.removeChild(inputElement);
+        });
 
     }
 
 
-    
+
+    const botao_copiar = document.getElementById("botao_copiar");
+    botao_copiar.addEventListener("click", copiar_texto_codificado);
+
     
 
+    
 });
-
-
-// function codificar(){
-//  var textoEntrada = document.getElementById("textoEntrada").value;
-//  var textoCodificado = encodeURIComponent(textoEntrada);
-//  console.log(textoEntrada);
-
 
 
 
